69cfe24566275868ef238fc83cad5f6a38e1ecdd
Jan Becker <jabe@gempa.de>
2016-06-22 09:08:05 +0200

    [trunk] Fix Spread connection error propagation

--- src/trunk/libs/seiscomp3/core/status.cpp
+++ src/trunk/libs/seiscomp3/core/status.cpp
@@ -13,41 +13,34 @@
 
 #include <seiscomp3/core/status.h>
 
-namespace Seiscomp
-{
-namespace Core
-{
+
+namespace Seiscomp {
+namespace Core {
 
 
-const char* const Status::STATUS_CODES_NAMES[ SC_QUANTITY ] =
-    {
-		"SEISCOMP_SUCCESS",
-		"SEISCOMP_ERROR",
-		"SEISCOMP_INVALID_GROUP_ERROR",
-		"SEISCOMP_INVALID_CLIENT_NAME_ERROR",
-		"SEISCOMP_TIMEOUT_ERROR",
-		"SEISCOMP_MESSAGE_SIZE_ERROR",
-		"SEISCOMP_ARCHIVE_REQUEST_ERROR",
-		"SEISCOMP_CONNECT_ERROR",
-		"SEISCOMP_NETWORKING_ERROR",
-		"SEISCOMP_NOT_CONNECTED_ERROR",
-		"SEISCOMP_CLIENT_NAME_NOT_UNIQUE",
-		"SEISCOMP_WRONG_SERVER_VERSION",
-		"SEISCOMP_TOO_MANY_USERS"
-    };
+const char* const Status::STATUS_CODES_NAMES[ SC_QUANTITY ] = {
+	"SEISCOMP_SUCCESS",
+	"SEISCOMP_ERROR",
+	"SEISCOMP_INVALID_GROUP_ERROR",
+	"SEISCOMP_INVALID_CLIENT_NAME_ERROR",
+	"SEISCOMP_TIMEOUT_ERROR",
+	"SEISCOMP_MESSAGE_SIZE_ERROR",
+	"SEISCOMP_ARCHIVE_REQUEST_ERROR",
+	"SEISCOMP_CONNECT_ERROR",
+	"SEISCOMP_NETWORKING_ERROR",
+	"SEISCOMP_NOT_CONNECTED_ERROR",
+	"SEISCOMP_CLIENT_NAME_NOT_UNIQUE",
+	"SEISCOMP_WRONG_SERVER_VERSION",
+	"SEISCOMP_TOO_MANY_USERS"
+};
 
 
-
-// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-const char* Status::StatusToStr(const int statusCode)
-{
+const char* Status::StatusToStr(const int statusCode) {
 	if (statusCode >= 0 && statusCode < SC_QUANTITY)
 		return STATUS_CODES_NAMES[ statusCode ];
 	return "Unknown status code";
 }
-// <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
 
 
 } // namespace Core
 } // namespace seiscomp3
-
--- src/trunk/libs/seiscomp3/core/status.h
+++ src/trunk/libs/seiscomp3/core/status.h
@@ -10,71 +10,72 @@
  *   SeisComP Public License for more details.                             *
  ***************************************************************************/
 
+
 #ifndef __SEISCOMP_CORE_STATUS__
 #define __SEISCOMP_CORE_STATUS__
 
+
 #include <seiscomp3/core.h>
+
 
 namespace Seiscomp {
 namespace Core {
 
+
 /**
  * Seiscomp status codes definitions.
  */
-class SC_SYSTEM_CORE_API Status
-{
-
+class SC_SYSTEM_CORE_API Status {
 	// -------------------------------------------------------------------
 	// Status codes
 	// -------------------------------------------------------------------
-public:
-	enum STATUS_CODES
-	{
-	    SEISCOMP_SUCCESS,
-	    SEISCOMP_FAILURE,
-		SEISCOMP_INVALID_GROUP_ERROR,
-		SEISCOMP_INVALID_CLIENT_NAME_ERROR,
-		SEISCOMP_TIMEOUT_ERROR,
-		SEISCOMP_MESSAGE_SIZE_ERROR,
-		SEISCOMP_ARCHIVE_REQUEST_ERROR,
-		SEISCOMP_CONNECT_ERROR,
-		SEISCOMP_NETWORKING_ERROR,
-		SEISCOMP_NOT_CONNECTED_ERROR,
-		SEISCOMP_CLIENT_NAME_NOT_UNIQUE,
-		SEISCOMP_WRONG_SERVER_VERSION,
-		SEISCOMP_TOO_MANY_USERS,
-	    SC_QUANTITY
-	};
+	public:
+		enum STATUS_CODES {
+			SEISCOMP_SUCCESS,
+			SEISCOMP_FAILURE,
+			SEISCOMP_INVALID_GROUP_ERROR,
+			SEISCOMP_INVALID_CLIENT_NAME_ERROR,
+			SEISCOMP_TIMEOUT_ERROR,
+			SEISCOMP_MESSAGE_SIZE_ERROR,
+			SEISCOMP_ARCHIVE_REQUEST_ERROR,
+			SEISCOMP_CONNECT_ERROR,
+			SEISCOMP_NETWORKING_ERROR,
+			SEISCOMP_NOT_CONNECTED_ERROR,
+			SEISCOMP_CLIENT_NAME_NOT_UNIQUE,
+			SEISCOMP_WRONG_SERVER_VERSION,
+			SEISCOMP_TOO_MANY_USERS,
+			SC_QUANTITY
+		};
 
 
 	// ---------------------------------------------------------------
 	// Public interfcae
 	// ---------------------------------------------------------------
-public:
-
-	/** Returns the string representation of the passed seiscomp error code
-	 * @return error code string representaion
-	 */
-	static const char* StatusToStr(const int statusCode);
+	public:
+		/**
+		 * Returns the string representation of the passed seiscomp error code
+		 * @return error code string representaion
+		 */
+		static const char* StatusToStr(const int statusCode);
 
 
 	// ---------------------------------------------------------------
 	// X'struction
 	// ---------------------------------------------------------------
-private:
-	Status() {}
+	private:
+		Status() {}
 
 
 	// ---------------------------------------------------------------
 	// Private data memebers
 	// ---------------------------------------------------------------
-private:
-	static const char* const STATUS_CODES_NAMES[];
-	
+	private:
+		static const char* const STATUS_CODES_NAMES[];
 };
 
 
 } // namespace Core
 } // namespace Seiscomp
+
 
 #endif
