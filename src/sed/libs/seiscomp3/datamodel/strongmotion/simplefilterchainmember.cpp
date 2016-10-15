0ebd49d2c8ab5bdf87dd03679919bcf9805d4f04
Jan Becker <jabe@gempa.de>
2016-10-15 22:13:57 +0200

    [sed] Make datamodel extension work with v0.9

--- src/sed/libs/seiscomp3/datamodel/strongmotion/simplefilterchainmember.cpp
+++ src/sed/libs/seiscomp3/datamodel/strongmotion/simplefilterchainmember.cpp
@@ -312,7 +312,7 @@ void SimpleFilterChainMember::accept(Visitor* visitor) {
 void SimpleFilterChainMember::serialize(Archive& ar) {
 	// Do not read/write if the archive's version is higher than
 	// currently supported
-	if ( ar.isHigherVersion<0,8>() ) {
+	if ( ar.isHigherVersion<0,9>() ) {
 		SEISCOMP_ERROR("Archive version %d.%d too high: SimpleFilterChainMember skipped",
 		               ar.versionMajor(), ar.versionMinor());
 		ar.setValidity(false);
--- src/sed/libs/seiscomp3/datamodel/strongmotion/strongmotionparameters.cpp
+++ src/sed/libs/seiscomp3/datamodel/strongmotion/strongmotionparameters.cpp
@@ -641,7 +641,7 @@ bool StrongMotionParameters::removeStrongOriginDescription(size_t i) {
 void StrongMotionParameters::serialize(Archive& ar) {
 	// Do not read/write if the archive's version is higher than
 	// currently supported
-	if ( ar.isHigherVersion<0,8>() ) {
+	if ( ar.isHigherVersion<0,9>() ) {
 		SEISCOMP_ERROR("Archive version %d.%d too high: StrongMotionParameters skipped",
 		               ar.versionMajor(), ar.versionMinor());
 		ar.setValidity(false);
