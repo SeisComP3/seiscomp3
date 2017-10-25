fa529b883503b5c9cab89b8ad0ca4e4455eb6940
Jan Becker <jabe@gempa.de>
2017-10-25 13:13:47 +0200

    [trunk] Replace spaces with tabs in PostgreSQL migration script

--- src/trunk/libs/seiscomp3/datamodel/share/migrations/postgresql/0_7_to_0_8.sql
+++ src/trunk/libs/seiscomp3/datamodel/share/migrations/postgresql/0_7_to_0_8.sql
@@ -1,20 +1,20 @@
 CREATE TABLE ResponseFAP (
-  _oid BIGINT NOT NULL,
-  _parent_oid BIGINT NOT NULL,
-  _last_modified TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-  m_name VARCHAR(255),
-  m_gain DOUBLE PRECISION,
-  m_gainFrequency DOUBLE PRECISION,
-  m_numberOfTuples SMALLINT,
-  m_tuples_content BYTEA,
-  m_tuples_used BOOLEAN NOT NULL DEFAULT '0',
-  m_remark_content BYTEA,
-  m_remark_used BOOLEAN NOT NULL DEFAULT '0',
-  PRIMARY KEY(_oid),
-  FOREIGN KEY(_oid)
-    REFERENCES Object(_oid)
-    ON DELETE CASCADE,
-  UNIQUE(_parent_oid,m_name)
+	_oid BIGINT NOT NULL,
+	_parent_oid BIGINT NOT NULL,
+	_last_modified TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+	m_name VARCHAR(255),
+	m_gain DOUBLE PRECISION,
+	m_gainFrequency DOUBLE PRECISION,
+	m_numberOfTuples SMALLINT,
+	m_tuples_content BYTEA,
+	m_tuples_used BOOLEAN NOT NULL DEFAULT '0',
+	m_remark_content BYTEA,
+	m_remark_used BOOLEAN NOT NULL DEFAULT '0',
+	PRIMARY KEY(_oid),
+	FOREIGN KEY(_oid)
+		REFERENCES Object(_oid)
+		ON DELETE CASCADE,
+UNIQUE(_parent_oid,m_name)
 );
 
 UPDATE Meta SET value='0.8' WHERE name='Schema-Version';
