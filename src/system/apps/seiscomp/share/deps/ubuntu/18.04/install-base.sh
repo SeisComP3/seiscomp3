ea92dad717a43d7967b57096a0cba77e67e8f4f9
Jan Becker <jabe@gempa.de>
2018-06-14 15:13:00 +0200

    [system] Add dependency scripts for Ubuntu 18.04

---
+++ src/system/apps/seiscomp/share/deps/ubuntu/18.04/install-base.sh
@@ -0,0 +1,1 @@
+apt-get install libxml2 libboost-filesystem1.62.0 libboost-iostreams1.62.0 libboost-thread1.62.0 libboost-program-options1.62.0 libboost-regex1.62.0 libboost-signals1.62.0 libboost-system1.62.0 libssl1.0.0 libncurses5 libmysqlclient20 libpq5 libpython2.7 python-m2crypto
