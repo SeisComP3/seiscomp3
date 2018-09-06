0e46193bb435a52c9efbceb4094d1628b993c348
Jan Becker <jabe@gempa.de>
2018-09-06 11:40:35 +0200

    [system] Fix Ubuntu 18.04 dependencies

--- src/system/apps/seiscomp/share/deps/ubuntu/18.04/install-base.sh
+++ src/system/apps/seiscomp/share/deps/ubuntu/18.04/install-base.sh
@@ -1,1 +1,1 @@
-apt-get install libxml2 libboost-filesystem1.62.0 libboost-iostreams1.62.0 libboost-thread1.62.0 libboost-program-options1.62.0 libboost-regex1.62.0 libboost-signals1.62.0 libboost-system1.62.0 libssl1.0.0 libncurses5 libmysqlclient20 libpq5 libpython2.7 python-m2crypto
+apt-get install libxml2 libboost-filesystem1.65.1 libboost-iostreams1.65.1 libboost-thread1.65.1 libboost-program-options1.65.1 libboost-regex1.65.1 libboost-signals1.65.1 libboost-system1.65.1 libssl1.0.0 libncurses5 libmysqlclient20 libpq5 libpython2.7 python-m2crypto
