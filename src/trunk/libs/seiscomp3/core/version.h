1d5c05dcddec7436f96ddff5db340bdc50f12a05
Jan Becker <jabe@gempa.de>
2019-03-08 13:52:01 +0100

    [gui] Add more RecordWidget query methods

--- src/trunk/libs/seiscomp3/core/version.h
+++ src/trunk/libs/seiscomp3/core/version.h
@@ -40,6 +40,10 @@ namespace Core {
  ******************************************************************************
  "12.1.0"   0x0C0100
    - Added ArtificialEventParametersMessage
+   - Fixed RecordWidget emitting of traceUpdated signal if the record slot to
+     be shown has changed
+   - Added non-const RecordWidget::traceInfo method
+   - Added RecordWidget::recordPen(int) method
 
  "12.0.0"   0x0C0000
    - Added Seiscomp::Core::Generic::Archive property interface
