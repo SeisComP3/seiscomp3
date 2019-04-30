96b0017804714e869b20732da0f2f2f44bddafc9
Jan Becker <jabe@gempa.de>
2019-04-30 15:02:52 +0200

    [trunk] Replaced old license text

--- src/trunk/libs/seiscomp3/processing/amplitudes/mwp_utils.c
+++ src/trunk/libs/seiscomp3/processing/amplitudes/mwp_utils.c
@@ -1,32 +1,15 @@
 /***************************************************************************
-* Copyright (C) 2009 BY GFZ Potsdam,                                       *
-* EMail: seiscomp-devel@gfz-potsdam.de                                     * 
-* The term "Non-Commercial Entity" is limited to the following:            *
-* - Research institutes                                                    *
-* - Public institutes dealing with natural hazards warnings                * 
-*                                                                          *
-* Provided that you qualify for a Non-Commercial Version License as        *
-* specified above, and subject to the terms and conditions contained       *
-* herein,GFZ Potsdam hereby grants the licensee, acting as an end user,    *
-* an institute wide, non-transferable, non-sublicensable license, valid    *
-* for an unlimited period of time, to install and use the Software, free   *
-* of charge, for non-commercial purposes only. The licensee is allowed     *
-* to develop own software modules based on the SeisComP3 libraries         *
-* licensed under GPL and connect them with software modules under this     *
-* license. The number of copies is for the licensee not limited within     *
-* the user community of the licensee.                                      *
-*                                                                          *
-* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS  *
-* OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF               *
-* MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.   *
-* IN NO EVENT SHALL THE CONTRIBUTORS OR COPYRIGHT HOLDERS BE LIABLE FOR    * 
-* ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, * 
-* TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE        *
-* SOFTWARE OR THE USE OR OTHER DEALINGS WITH THE SOFTWARE.                 *
-*                                                                          * 
-* GFZ Potsdam reserve all rights not expressly granted to the licensee     *
-* herein. Find more details of the license at geofon.gfz-potsdam.de        *
-***************************************************************************/
+ *   Copyright (C) by GFZ Potsdam                                          *
+ *                                                                         *
+ *   You can redistribute and/or modify this program under the             *
+ *   terms of the SeisComP Public License.                                 *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   SeisComP Public License for more details.                             *
+ ***************************************************************************/
+
 
 #include<math.h>
 #include "mwp_utils.h"
