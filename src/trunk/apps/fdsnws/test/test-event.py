3e10b34b1295b293925a5cbce8cedb3103cdb0a8
Stephan Herrnkind <herrnkind@gempa.de>
2019-08-26 09:02:18 +0200

    [fdsnws] Bug fixes and test cases

---
+++ src/trunk/apps/fdsnws/test/test-event.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+
+###############################################################################
+# Copyright (C) 2013-2014 by gempa GmbH
+#
+# Author:  Stephan Herrnkind
+# Email:   herrnkind@gempa.de
+###############################################################################
+
+from __future__ import print_function
+
+import sys
+
+from fdsnwstest import FDSNWSTest
+
+
+###############################################################################
+class TestEvent(FDSNWSTest):
+
+    #--------------------------------------------------------------------------
+    def test(self):
+        print('Testing event service')
+
+        query = '{}/event/1/query'.format(self.url)
+        ctTXT = 'text/plain'
+        ctXML = 'application/xml'
+        resFile = self.rootdir + '/results/event-'
+
+        i = 1
+        tests = [
+            (ctTXT, '?format=text'),
+            (ctTXT, '?format=text&minmag=3&mindepth=20&minlon=150'),
+            (ctTXT, '?format=text&eventtype=earthquake,unknown'),
+            (ctXML, '?format=sc3ml&eventid=rs2019qsodmc&formatted=true'),
+            (ctXML, '?format=sc3ml&includeallorigins=true' \
+                    '&includeallmagnitudes=true&includearrivals=true' \
+                    '&includefocalmechanism=true&includestationmts=true' \
+                    '&includecomments=true&eventid=rs2019qsodmc' \
+                    '&formatted=true'),
+            (ctXML, ''),
+            (ctXML, '?format=qml-rt'),
+            (ctTXT, '?format=csv'),
+        ]
+        for test in tests:
+            self.testGET('{}{}'.format(query, test[1]), test[0],
+                         dataFile='{}{}.txt'.format(resFile, i), testID=i)
+            i += 1
+
+
+
+
+#------------------------------------------------------------------------------
+if __name__ == '__main__':
+    app = TestEvent()
+    sys.exit(app())
+
+
+
+# vim: ts=4 et tw=79
---
+++ src/trunk/apps/fdsnws/test/test-station.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+
+###############################################################################
+# Copyright (C) 2013-2014 by gempa GmbH
+#
+# Author:  Stephan Herrnkind
+# Email:   herrnkind@gempa.de
+###############################################################################
+
+from __future__ import print_function
+
+import sys
+
+from fdsnwstest import FDSNWSTest
+
+
+###############################################################################
+class TestStation(FDSNWSTest):
+
+    #--------------------------------------------------------------------------
+    def test(self):
+        print('Testing station service')
+
+        query = '{}/station/1/query'.format(self.url)
+        ctTXT = 'text/plain'
+        ctXML = 'application/xml'
+        resFile = self.rootdir + '/results/station-'
+
+        i = 1
+        tests = [
+            (ctTXT, '?format=text&level=channel'),
+            (ctTXT, '?format=text&includerestricted=false'),
+            (ctTXT, '?format=text&startbefore=2019-07-01'),
+            (ctXML, '?level=channel&includeavailability=true', [(172,197)]),
+            (ctXML, '?format=sc3ml&network=AM&station=R0F05&location=00' \
+                    '&channel=SHZ&latitude=52&longitude=13&maxradius=0.5' \
+                    '&level=response&includeavailability=true'),
+        ]
+        for test in tests:
+            ignoreRanges = [] if len(test) < 3 else test[2]
+            self.testGET('{}{}'.format(query, test[1]), test[0],
+                         dataFile='{}{}.txt'.format(resFile, i), testID=i,
+                         ignoreRanges=ignoreRanges)
+            i += 1
+
+
+
+
+#------------------------------------------------------------------------------
+if __name__ == '__main__':
+    app = TestStation()
+    sys.exit(app())
+
+
+
+# vim: ts=4 et tw=79
