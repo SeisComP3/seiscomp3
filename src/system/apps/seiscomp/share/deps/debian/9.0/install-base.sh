a580526bf3d4b90a627d59938a4b2c37954a4617
Jan Becker <jabe@gempa.de>
2020-01-28 08:45:35 +0100

    [system] Add dependencies for debian 9 and 10

---
+++ src/system/apps/seiscomp/share/deps/debian/9.0/install-base.sh
@@ -0,0 +1,1 @@
+apt install libxml2 libboost-filesystem1.62.0 libboost-iostreams1.62.0 libboost-thread1.62.0 libboost-program-options1.62.0 libboost-regex1.62.0 libboost-signals1.62.0 libboost-system1.62.0 libssl1.0.2 libncurses5 libmariadb2 libpq5 libpython2.7 python-m2crypto
