20d091751b3b99e6840db071b79ab5ddfb5c3fd6
Jan Becker <jabe@gempa.de>
2020-04-24 12:35:42 +0200

    [scevent] Release the event created by an delayed origin immediately

--- src/trunk/apps/processing/scevent/eventtool.h
+++ src/trunk/apps/processing/scevent/eventtool.h
@@ -78,7 +78,8 @@ class EventTool : public Application {
 		bool handleJournalEntry(DataModel::JournalEntry *);
 
 		EventInformationPtr associateOriginCheckDelay(DataModel::Origin *);
-		EventInformationPtr associateOrigin(DataModel::Origin *, bool allowEventCreation);
+		EventInformationPtr associateOrigin(DataModel::Origin *, bool allowEventCreation,
+		                                    bool *createdEvent = 0);
 		void updatedOrigin(DataModel::Origin *, DataModel::Magnitude *, bool realOriginUpdate);
 
 		EventInformationPtr associateFocalMechanismCheckDelay(DataModel::FocalMechanism *);
