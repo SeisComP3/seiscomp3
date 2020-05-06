a1ef201ea119051e5b75f6d59c22ea14da2194f0
Andres Heinloo <andres@gfz-potsdam.de>
2020-05-07 00:20:00 +0200

    [seedlink] [scream] Remove debugging output

--- src/seedlink/plugins/scream_plugin/dispatch.c
+++ src/seedlink/plugins/scream_plugin/dispatch.c
@@ -80,14 +80,14 @@ dispatch (gcf_block b, int recno)
 	     {
 	          if (strncmp(b->sysid, mp->sysid, strlen(mp->sysid)) == 0)
 		  {
-		       printf("Sending raw epoch: %s.%s.%s @ %d Hz\n", mp->network, mp->station, mp->channel, b->sample_rate);
+		       /*printf("Sending raw epoch: %s.%s.%s @ %d Hz\n", mp->network, mp->station, mp->channel, b->sample_rate);*/
                        send_raw_depoch ( sta_id, mp->channel, b->estart, 0, -1, b->data, b->samples);
                        chid=mp->id;
 		  }
 	     }
 	     else
 	     {
-	         printf("Sending raw epoch: %s.%s @ %d Hz\n", mp->station, mp->channel, b->sample_rate);
+	         /*printf("Sending raw epoch: %s.%s @ %d Hz\n", mp->station, mp->channel, b->sample_rate);*/
                  send_raw_depoch ( sta_id, mp->channel, b->estart, 0, -1, b->data, b->samples);
                  chid=mp->id;
 	     }
